65bb6e2a801d201e396b08e310ed26a1b0ab7892
shoooya <shoooya17@gmail.com>
2018-12-30 22:46:13 +0900

    Initial Commit

---
+++ day1_1-3-1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  day1_1-3-1
+//
+//  Created by しょーや on 2018/12/30.
+//  Copyright © 2018 HelloWorlders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
